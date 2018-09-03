d1fca39bafa7b1ad70895c5de128f88d55be4a1e
cjuul <42136084+clausjuul@users.noreply.github.com>
2018-09-03 17:44:10 +0200

    tilmeld + lidt andet

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,19 +1,36 @@
-const body =  document.querySelector("body");
+const body = document.querySelector("body");
 
-   document.getElementById("mobile-menu").onclick = function() {
-    
-    if(!body.classList.contains("toggle-menu")) {
-body.classList.add("toggle-menu");
+document.getElementById("mobile-menu").onclick = function () {
+
+    if (!body.classList.contains("toggle-menu")) {
+        body.classList.add("toggle-menu");
 
 
-} else {
+    } else {
 
-body.classList.remove("toggle-menu");
-} 
+        body.classList.remove("toggle-menu");
+    }
 }
 
 function overlay() {
-    if(body.classList.contains("toggle-menu")) {
+    if (body.classList.contains("toggle-menu")) {
         body.classList.remove("toggle-menu");
-       }
+    }
+}
+
+//show tilmeld
+document.getElementById("show-tilmeld").onclick = function () {
+
+    if (!body.classList.contains("toggle-tilmeld")) {
+        body.classList.add("toggle-tilmeld");
+
+    }
+}
+//close tilmeld
+document.getElementById("close-tilmeld").onclick = function () {
+
+    if (body.classList.contains("toggle-tilmeld")) {
+        body.classList.remove("toggle-tilmeld");
+
+    }
 }
